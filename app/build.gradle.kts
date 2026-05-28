291fe4546209c6a5a735763a184e20c344dce5b5
KiraIshb <163666090+KiraIshb@users.noreply.github.com>
2026-05-28 09:39:11 +0500

    home screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation("com.google.accompanist:accompanist-pager:0.35.0-alpha")
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.35.0-alpha")
 }
